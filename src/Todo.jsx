029951155112bfeae2e1f90b497bcc3eea1c36ff
amanrkt2020@gmail.com <amanrkt2020@gmail.com>
2024-11-24 15:52:15 +0530

    add TaskViewer component and implement task deletion; update dependencies

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,20 +1,34 @@
 import { useState } from 'react';
 import { Input } from './components/Input';
+import { TaskViewer } from './components/TaskViewer';
 export const Todo = () => {
   const [inputValue, setInputValue] = useState('');
   const [task, setTask] = useState([]);
-  
+  const handleDelete = (index) => {
+    setTask((prevTask) => {
+      return prevTask.filter((_, i) => i !== index);
+    });
+  };
 
   return (
-    <div>
-      <h1>Todo</h1>
-      <Input 
-        inputValue={ inputValue }
-        setInputValue={ setInputValue }
-        task={ task }
-        setTask={ setTask }
-      />
+    <>
+      <div>
+        <h1>Todo</h1>
+        <Input
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          task={task}
+          setTask={setTask}
+        />
+      </div>
+      <div>
+        <TaskViewer 
+          tasks={task}
+          deleteTask={(index) => handleDelete(index)}
+        />
+      </div>
+    </>
 
-    </div>
+
   )
 }
